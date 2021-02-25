92a9f2ba7037915eb781892f4fdff974022f9f3b
MUHAMMAD IBRAHIM UMAR <muhammadbmw69@gmail.com>
2021-02-25 06:38:09 +0100

    Delete Second-Rivision.js

--- Second-Rivision.js
+++
@@ -1,8 +0,0 @@
-<!--
-Visual Code Mobile
-Developed By Manish Nirmal
-App Available on Play Store :-
-https://play.google.com/store/apps/details?id=lk.visual.code.mobile
-YouTube :-
-https://youtube.com/ManishNirmal
--->
